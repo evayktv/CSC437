8b855acb76cdb82a40f166dd7a35af20c5cd9725
evakytv <evancao419@gmail.com>
2025-10-25 18:27:32 -0700

    Implement client side rendering

--- packages/proto/src/main.ts
+++ packages/proto/src/main.ts
@@ -1,6 +1,8 @@
 import { define } from "@calpoly/mustang";
 import { CarModelCardElement } from "./car-model-card";
+import { CarCatalogElement } from "./car-catalog";
 
 define({
   "car-model-card": CarModelCardElement,
+  "car-catalog": CarCatalogElement,
 });
